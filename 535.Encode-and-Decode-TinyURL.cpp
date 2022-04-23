9875140de34ea5be1ca9f1ee4959f37cb19deadd
Michael Ehab <michaelehab16@gmail.com>
2022-04-23 20:06:10 +0200

    Added Day 23 Solution

---
+++ 535.Encode-and-Decode-TinyURL.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    unordered_map<string, string> tmp;
+    int last = 0;
+public:
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        string s ="http://tinyurl.com/" + to_string(last++);
+        tmp[s] = longUrl;
+        return s;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return tmp[shortUrl];
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
